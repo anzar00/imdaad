f55510b80b63031668ee3ff8d6e99456cbcffb61
anzar00 <peerzada.anzar@gmail.com>
2022-06-12 04:39:10 +0530

    navbar + footer + hero

--- index.php
+++ index.php
@@ -12,3 +12,34 @@
   <?php include 'header.php' ?>
 
 </head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="#">Imdaad</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <?php include 'navbar.php'?>
+
+      <a href="#" class="get-started-btn">Get Started</a>
+
+    </div>
+  </header>
+  <!-- End Header -->    
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-content-center align-items-center">
+    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Learning Today,<br>Leading Tomorrow</h1>
+      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <a href="courses.html" class="btn-get-started">Get Started</a>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <?php include 'footer.php' ?>
+
+</body>
